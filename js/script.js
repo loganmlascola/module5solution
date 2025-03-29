9530a1194b6d94d08f65dc31a6e9b59e29ce457f
Logan LaScola <loganmlascola@gmail.com>
2025-03-28 21:29:32 -0700

    commit

--- js/script.js
+++ js/script.js
@@ -14,15 +14,12 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
-var allCategoriesUrl = "http://localhost:8000/categories";
-  //"https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
+var allCategoriesUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
-var menuItemHtml = "http://localhost:8000/menu_items/";
-
-//"snippets/menu-item.html";
+var menuItemHtml = "snippets/menu-item.html";
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
